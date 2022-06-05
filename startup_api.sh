70550efd1aaa34feff6aedb077929f3a2b9c0e92
victor bai <1355837170@qq.com>
2022-06-06 00:28:11 +0800

    update startup_api

--- startup_api.sh
+++ startup_api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ./textMG/api_run.py
+cd textMG python && gunicorn api_run:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 192.168.1.14:5000 --log-level debug
